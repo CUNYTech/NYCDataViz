fdb8bca6b853c8b071a473e06fa0a4670e377ebf
Jesaiah Prayor <jesaiahprayor@gmail.com>
2017-12-05 13:22:29 -0500

    Changing the home page to work mobily

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -420,33 +420,13 @@ function complaintType(){
     };
 
     request.send();
-
 }
 
-function myFunction (){
-  document.getElementById("dropDown").classList.toggle("show");
+function Toggle (){
+  var dropDown = document.getElementById('dropTop');
+      dropDown.classList.toggle('display');
 }
 
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
 
-/*DO I NEED THIS ???? */
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
 
